bb088efedd5a0e7040dd4156101c461a7b88595a
Ana Lívia da Costa Pessoa <analiviapessoa@uol.com.br>
2025-11-18 11:38:02 -0300

    Adding images

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,3 +1,23 @@
-import LogoCITi from "./Logo.png";
+// Pets
+import cat from "./Pets/cat.png"
+import cow from "./Pets/cow.png"
+import dog from "./Pets/dog.png"
+import horse from "./Pets/horse.png"
+import pig from "./Pets/pig.png"
+import sheep from "./Pets/sheep.png"
 
-export { LogoCITi };
+// Icons
+import add from "./Icons/add.png"
+import alarm from "./Icons/alarm.png"
+import arrow from "./Icons/arrow.png"
+import arrow_back from "./Icons/arrow_back.png"
+import arrow_down from "./Icons/arrow_down.png"
+import calendar from "./Icons/calendar.png"
+import check from "./Icons/check.png"
+import close from "./Icons/close.png"
+
+// Logos and others
+import LogoPet from "./Logos/LogoPet.png"
+import favorite from "./Logos/favorite.png"
+
+export { LogoPet, favorite, cat, cow, dog, horse, pig, sheep, alarm, arrow, arrow_down, arrow_back, check, close, calendar, add };
