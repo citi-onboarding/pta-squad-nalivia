2494e7150056d23bb581ba40b6ecb4aa81f2c0e8
sofiaavallone <sofiavallone17@gmail.com>
2025-11-23 12:24:33 -0300

    fix([header]): making it responsive and implementing tailwind

--- client/src/components/ui/Header/index.tsx
+++ client/src/components/ui/Header/index.tsx
@@ -7,43 +7,41 @@ import { usePathname } from 'next/navigation';
 import { LogoPet } from "@/assets";
 import { favorite } from "@/assets";
 import { LogoPurple } from "@/assets";
-import { Button } from '../button';
 import { cn } from "@/lib/utils";
 
 export default function Header() {
     const path = usePathname()
 
     return (
-        <div style={{width: '1920px', height: '114px', backgroundColor: '#D9D9D9', fontFamily: 'SF Pro Display', color: '#7D1AD7',
-                    display: 'flex', flexDirection: 'row', justifyContent: 'space-between', fontSize: '16px'}}> {/* Header geral */}
+        <div className='w-full h-auto bg-#F0F0F0 font-(family-name:--SF Pro Display) text-#7D1AD7 flex flex-row justify-between text-base border-b border-[#D9D9D9] px-5 py-auto'> {/* Header geral */}
             
             {/* Logo Pet */}
-            <div style={{width: '189px', height: '74px', marginLeft: '48px', marginTop: '20px', marginBottom: '20px'}}> 
+            <div className='w-[189px] h-auto ml-12 mt-5 mb-5'> 
                 <Image src={LogoPet} alt="Logo Pet" />
             </div>
 
             {/* NavBar */}
-            <div style={{width: '196px', height: '42px', marginTop: '36px', marginBottom: '36px', gap: '48px', display: 'flex', flexDirection: 'row', fontWeight: '400'}}> 
-                <Link href='/Atendimento' className={cn("px-1 py-3 text-[#242424]", path === "/Atendimento" && "border-b-2 border-[#50E678]")}>
+            <div className='w-[10.2%] h-[3.89%] mt-9 mb-9 gap-12 flex flex-row font-normal'> 
+                <Link href='/Atendimento' className={cn('px-1 py-3 text-[#242424]', path === '/Atendimento' && 'border-b-2 border-[#50E678]')}>
                     Atendimento
                 </Link>
 
-                <Link href='/Cadastro' className={cn("px-1 py-3 text-[#242424]", path === "/Cadastro" && "border-b-2 border-[#50E678]")}>
+                <Link href='/Cadastro' className={cn('px-1 py-3 text-[#242424]', path === '/Cadastro' && 'border-b-2 border-[#50E678]')}>
                     Cadastro
                 </Link>
             </div>
 
             {/* Frase lateral direta */}
-            <div style={{display: 'flex', flexDirection: 'row', marginRight: '48px', marginTop: '45px', marginBottom: '45px', gap: '4px', fontWeight: '400'}}> 
+            <div className='flex flex-row mr-12 mt-[45px] mb-[45px] gap-1 font-normal'> 
                 <p>made with</p>
 
-                <div style={{width: '24px', height: '24px', marginTop: '2.5px', marginBottom: '2.5px'}}>
+                <div className='w-[24px] h-auto mt-[2.5px] mb-[2.5px]'>
                     <Image src={favorite} alt="Coração" />
                 </div>
 
                 <p>and {'</>'} by</p>
 
-                <div style={{width: '41px', height: '21px', marginTop: '1.5px', marginBottom: '1.5px'}}>
+                <div className='w-[41px] h-auto mt-[1.5px] mb-[1.5px]'>
                     <Image src={LogoPurple} alt="Logo Citi Roxa" />
                 </div>
             </div>
