9e66bef474610e2e8f097ee13c77c5672998b59b
Ana Lívia da Costa Pessoa <analiviapessoa@uol.com.br>
2025-11-25 10:56:41 -0300

    fix/assets

--- mobile/src/assets/index.ts
+++ mobile/src/assets/index.ts
@@ -1,10 +1,10 @@
-import citi from './citi.svg';
+// Icons
+import cloud from './Icons/cloud.svg';
+import moon from './Icons/moon.svg';
+import sun from './Icons/sun.svg';
 
-import cloud from './cloud.svg';
-
-import moon from './moon.svg';
-
-import sun from './sun.svg';
+// Logos and others
+import citi from './Logos/citi.svg';
 
 export {
   citi, cloud, moon, sun
