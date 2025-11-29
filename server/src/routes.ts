30bde41df3f9ae830f5175fcef58050aab65dde0
fabio-uchoa <fabiohulopes@gmail.com>
2025-11-29 18:17:44 -0300

    feat/CrudConsult: Add ConsultController and respective routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import PatientController from "./controllers/PatientController";
+import ConsultController from "./controllers/ConsultController";
 
 const routes = express.Router();
 
@@ -9,4 +10,8 @@ routes.get("/patient", PatientController.get);
 routes.delete("/patient/:id", PatientController.delete);
 routes.patch("/patient/:id", PatientController.update);
 
+routes.post("/consult", ConsultController.create);
+routes.get("/consult", ConsultController.get);
+routes.delete("/consult/:id", ConsultController.delete);
+routes.patch("/consult/:id", ConsultController.update);
 export default routes;
